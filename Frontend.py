2c2fa7f88be7c66bb7e3520a73ef0624654202aa
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-01-04 19:34:35 +0300

    sth

--- Frontend.py
+++ Frontend.py
@@ -4,14 +4,65 @@ import Backend
 from Backend import *
 
 
+def get_selected_row(event):
+    global selected_tuple
+    index=list1.curselection()[0]
+    selected_tuple = list1.get(index)
+    e1.delete(0, END)
+    e1.insert(END,selected_tuple[0])
+    e2.delete(0, END)
+    e2.insert(END,selected_tuple[1])
+    e3.delete(0, END)
+    e3.insert(END,selected_tuple[3])
+    e4.delete(0, END)
+    e4.insert(END,selected_tuple[3])
+    e5.delete(0, END)
+    e5.insert(END,selected_tuple[4])
+    e6.delete(0, END)
+    e6.insert(END,selected_tuple[5])
+    e7.delete(0, END)
+    e7.insert(END,selected_tuple[6])
+    e8.delete(0, END)
+    e8.insert(END,selected_tuple[7])
+    e9.delete(0, END)
+    e9.insert(END,selected_tuple[8])
+    e10.delete(0, END)
+    e10.insert(END,selected_tuple[9])
+    e11.delete(0, END)
+    e11.insert(END,selected_tuple[10])
+    e12.delete(0, END)
+    e12.insert(END,selected_tuple[11])
+
+
 def view_command():
     list1.delete(0, END)
     for row in Backend.view_player():
         list1.insert(END, row)
 
+
 def search_command():
-    pass
-    ##TODO burda kaldımw
+    list1.delete(0,END)
+    for row in Backend.search_player(team_id_text.get(), first_name_text.get(),
+                                     last_name_text.get(), age_text.get(), salary_text.get(),
+                                     position_text.get(), weight_text.get(), height_text.get(),
+                                     avr_point_text.get(), avr_asist_text.get(), avr_rebound_text.get()):
+        list1.insert(END, row)
+
+
+def add_command():
+    Backend.insert_player(team_id_text.get(), first_name_text.get(),
+                          last_name_text.get(), age_text.get(), salary_text.get(),
+                          position_text.get(), weight_text.get(), height_text.get(),
+                          avr_point_text.get(), avr_asist_text.get(), avr_rebound_text.get())
+
+    list1.delete(0, END)
+    view_command()
+
+    
+def delete_command():
+    Backend.delete_player(selected_tuple[0])
+    view_command()
+
 
 window = Tk()
 window.wm_title('Players')
@@ -110,19 +161,21 @@ sb1.grid(row=6, column=2, rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
+list1.bind('<<ListboxSelect>>', get_selected_row)
+
 b1 = Button(window, text="View all", width=12, command=view_command)
 b1.grid(row=6, column=3)
 
 b2 = Button(window, text="Search", width=12, command=search_command)
 b2.grid(row=7, column=3)
 
-b3 = Button(window, text="Add", width=12)
+b3 = Button(window, text="Add", width=12, command=add_command)
 b3.grid(row=8, column=3)
 
 b4 = Button(window, text="Update", width=12)
 b4.grid(row=9, column=3)
 
-b5 = Button(window, text="Delete", width=12)
+b5 = Button(window, text="Delete", width=12, command=delete_command)
 b5.grid(row=10, column=3)
 
 b6 = Button(window, text="Close", width=12)
